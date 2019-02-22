f7fcfa8ac2a0f3f7b6aab90aa16662ee2f9ce5cc
YSKang <ysck73@naver.com>
2019-02-22 20:50:38 +0900

    install-unity의 UNITY_WINDOWS_TARGET_PACKAGE_URL을 현재 사용이 가능한 url로 수정

--- travis-build/install-unity.sh
+++ travis-build/install-unity.sh
@@ -4,7 +4,7 @@
 # to get download URLs
 UNITY_DOWNLOAD_CACHE="$(pwd)/unity_download_cache"
 UNITY_OSX_PACKAGE_URL="https://download.unity3d.com/download_unity/88933597c842/MacEditorInstaller/Unity-2018.2.17f1.pkg"
-UNITY_WINDOWS_TARGET_PACKAGE_URL="http://download.unity3d.com/download_unity/88933597c842/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-2018.2.17f1.pkg"
+UNITY_WINDOWS_TARGET_PACKAGE_URL="https://download.unity3d.com/download_unity/88933597c842/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-2018.2.17f1.pkg"
 
 
 # Downloads a file if it does not exist
