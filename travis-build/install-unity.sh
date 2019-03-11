1f256ca1ce0bc4e924d23df0baf96792598f28c9
YSKang <ysck73@naver.com>
2019-03-11 12:25:34 +0900

    android sdkmanager yes 옵션 추가

--- travis-build/install-unity.sh
+++ travis-build/install-unity.sh
@@ -72,6 +72,6 @@ export ANDROID_NDK_HOME="/usr/local/share/android-ndk"
 echo "executing: sdkmanager --update"
 yes y | ${ANDROID_SDK_ROOT}/tools/bin/sdkmanager --update
 #yes y | ${ANDROID_SDK_ROOT}/tools/bin/sdkmanager "build-tools;${androidBuildToolsVersion}" "platform-tools" "platforms;${androidPlatformVerion}"
-${ANDROID_SDK_ROOT}/tools/bin/sdkmanager "build-tools;${androidBuildToolsVersion}" "platform-tools" "platforms;${androidPlatformVerion}"
+yes y | ${ANDROID_SDK_ROOT}/tools/bin/sdkmanager "build-tools;${androidBuildToolsVersion}" "platform-tools" "platforms;${androidPlatformVerion}"
 
 
